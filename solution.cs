75150f86059e3d950cf88968eba6084ef00abf2e
Kooreis <kooreis.official@gmail.com>
2023-11-08 13:35:28 -0600

    Adds a for loop to iterate over the array. If an element is not yet in the countMap, it adds it with a count of 1. If it is already in the countMap, it increments the count.

--- solution.cs
+++ solution.cs
@@ -1,5 +1,7 @@
-function findMajorityElement(arr) {
-    let countMap = {};
-    let majorityElement = null;
-    let majorityCount = 0;
+for (let i = 0; i < arr.length; i++) {
+        if (!countMap[arr[i]]) {
+            countMap[arr[i]] = 1;
+        } else {
+            countMap[arr[i]]++;
+        }
 }
